e5ba80980111068007eecf5319bf3ba4f9379726
allegro0132 <34514398+allegro0132@users.noreply.github.com>
2024-03-04 16:41:57 +0800

    Add Python package upload workflow and update version number

--- supergrad/__init__.py
+++ supergrad/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "0.1.0"
+
 from .utils import Helper, identity_wrap, tensor, basis, permute
 from .quantum_system import Kronobj, LindbladObj
 from .common_functions import Spectrum, Evolve
