824f99d5b20be95eb5ab44d0a7280c63fb777a42
Feng Wu <wufeng@iqubit.org>
2024-07-05 18:19:48 +0800

    Add a thread-based memory profiler

--- supergrad/utils/memory_profiling.py
+++ supergrad/utils/memory_profiling.py
@@ -5,40 +5,52 @@ import os
 import sys
 import signal
 import subprocess
+import platform
+from threading import Thread
+
+
+class RecordMaxMemoryUsage(Thread):
+    def __init__(self, pid):
+        super().__init__()
+        self.pid = pid
+        self.ps_process = psutil.Process(pid)
+        self.max_mem = 0
+        self.stop = False
+
+    def run(self):
+        while self.ps_process.is_running() and not self.stop:
+            try:
+                # Get the memory info
+                mem_info = self.ps_process.memory_info()
+
+                # Update max memory usage
+                self.max_mem = max(self.max_mem, mem_info.rss)
+
+                # Sleep for a while
+                time.sleep(0.01)
+
+            except psutil.NoSuchProcess:
+                break
+
+    def get_max_mem(self):
+        return self.max_mem
+
+    def set_stop(self):
+        self.stop = True
 
 
 def max_memory_usage(pid):
-    # Create a psutil process object
-    ps_process = psutil.Process(pid)
+    t0 = RecordMaxMemoryUsage(pid=pid)
+    t0.start()
 
-    # Initialize max memory usage as 0
-    max_mem = 0
+    # Wait input
+    input()
 
-    def handle_sigint(sig, frame):
-        # Print the max memory usage
-        print(max_mem)
-        sys.exit(0)
-
-    # Set the SIGINT handler
-    signal.signal(signal.SIGINT, handle_sigint)
-
-    # While the process is running
-    while ps_process.is_running():
-        try:
-            # Get the memory info
-            mem_info = ps_process.memory_info()
-
-            # Update max memory usage
-            max_mem = max(max_mem, mem_info.rss)
-
-            # Sleep for a while
-            time.sleep(0.01)
-
-        except psutil.NoSuchProcess:
-            break
+    t0.set_stop()
+    t0.join()
 
     # Print the max memory usage
-    print(max_mem)
+    print(t0.max_mem)
 
 
 def trace_max_memory_usage(func, pid):
@@ -46,14 +58,14 @@ def trace_max_memory_usage(func, pid):
     def wrapper(*args, **kwargs):
         process = subprocess.Popen(
             ['python', __file__, str(pid)],
+            stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE)
 
         output = func(*args, **kwargs)
         # Stop the memory profiling process
-        os.kill(process.pid, signal.SIGINT)
         # Get the stdout and stderr
-        stdout_data, stderr_data = process.communicate()
+        stdout_data, stderr_data = process.communicate(input=b"stop\n")
         # delete \n in the end of the stdout
         stdout_data = stdout_data.decode('utf-8').strip()
         return output, int(stdout_data)
