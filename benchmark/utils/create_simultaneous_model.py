7b5e8bdb5871440366ae4772a8fb7a0017259c8f
allegro0132 <34514398+allegro0132@users.noreply.github.com>
2024-10-11 11:35:20 +0800

    Add Hadamard transform examples

--- benchmark/utils/create_simultaneous_model.py
+++ benchmark/utils/create_simultaneous_model.py
@@ -2,6 +2,29 @@ from supergrad.helper import Evolve
 from supergrad.scgraph.graph_mpc_fluxonium_1d import MPCFluxonium1D
 
 
+def create_hadamard_transform(n_qubit,
+                              astep,
+                              trotter_order,
+                              diag_ops,
+                              minimal_approach=False,
+                              custom_vjp=None):
+    chain = MPCFluxonium1D(n_qubit, periodic=False, seed=42)
+    chain.create_single_qubit_pulse(range(n_qubit), [50.0] * n_qubit,
+                                    True,
+                                    factor=0.25,
+                                    minimal_approach=minimal_approach)
+
+    return Evolve(chain,
+                  truncated_dim=2,
+                  compensation_option='no_comp',
+                  options={
+                      'astep': astep,
+                      'trotter_order': trotter_order,
+                      'diag_ops': diag_ops,
+                      'custom_vjp': custom_vjp
+                  })
+
+
 def create_simultaneous_x(n_qubit,
                           astep,
                           trotter_order,
